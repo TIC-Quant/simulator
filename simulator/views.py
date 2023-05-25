c03e2d5306fc38b66951e9e52a54ef97cba1b10e
cosmos1030 <dyk6208@gmail.com>
2023-05-25 11:54:08 +0000

    still fixing nnone error

--- simulator/views.py
+++ simulator/views.py
@@ -4,8 +4,9 @@ import cosi
 # Create your views here.
 def index(request):
     performance = get_performance()
+    # print(performance)
     return render(request, 'simulator/index.html',{
-        "performance": performance
+        "performance": "아아악"
     })
 
 def get_performance():
@@ -15,5 +16,5 @@ def get_performance():
     cosi.indicator_to_signal(df, factor='rsi', buy=30, sell=70)
     cosi.position(df)
     cosi.evaluate(df, cost=.001)
-    print(cosi.performance(df, rf_rate=0.01))
+    print("으악"+cosi.performance(df, rf_rate=0.01))
     return cosi.performance(df, rf_rate=0.01)
